7d3db973761a2837f3c0392d46cada27ababed7b
Ladicii <carcheresuah@gmail.com>
2023-05-07 20:09:40 +0000

    3. alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,14 +8,14 @@
 int main(void)
 {
 int b = 97;
-int c = 65;
+int B = 65;
 while (b <= 122)
 {
 	putchar(b);
 }
-while (c <= 90)
+while (B <= 90)
 {
-	putchar(c);
+	putchar(B);
 }
 putchar('\n');
 return (0);
