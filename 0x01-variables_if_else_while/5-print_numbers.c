5bd0878cc35516dd0aa00f9f755de6f9395e06e0
Ladicii <carcheresuah@gmail.com>
2023-05-07 20:42:54 +0000

    5. Numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,1 +1,16 @@
-we
+#include <stdio.h>
+/**
+ * main - Print numbers of base 10
+ * Return: Always 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		printf("%d", a);
+	}
+	printf("\n");
+	return (0);
+}
