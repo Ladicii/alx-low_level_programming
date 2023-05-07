d63620eea3c3dfc09ddd42fc078be6e889bbada4
Ladicii <carcheresuah@gmail.com>
2023-05-07 18:27:35 +0000

    4. When I was having that alphabet soup, I never thought that it would pay off

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdib.h>
 #include <string.h>
 /**
  * main -print the alphabet without q and e.
@@ -9,7 +8,7 @@
 int main(void)
 {
 int i;
-for (i = 90 : i < 123 : i++)
+for (i = 97 ; i < 123 ; i++)
 {
 if (i != 101 && i != 113)
 {
