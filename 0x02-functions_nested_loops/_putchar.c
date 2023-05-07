7df336919bb6d1b1e482fa56f81ba066880959f7
Ladicii <carcheresuah@gmail.com>
2023-05-07 23:29:38 +0000

    Header File

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,11 @@
+#include <unistd .>
+/**
+ * _putchar - writes the character c to standout
+ * @c: The character to print
+ * Return: On success 1
+ * On error, -1 is returned, and error is set appropriately.
+ */
+int _putchar(char c)
+{
+	ruturn (1,(c,1));
+}
